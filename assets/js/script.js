3d42fbcb166720e9489e14da8ec94cf8bca16c73
Tyler Olson <t19.olson@gmail.com>
2022-09-21 10:02:31 -0500

    fixed answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,56 +9,7 @@ var score = 0;
 var i = 0;
 
 
-
-//Timer   WORKING, Hooray!
-
-function countdown() {
-  var timeLeft = 120;
-
-  var timerInterval = setInterval(function() {
-    timeLeft--;
-    timeElement.textContent = timeLeft;
-
-    if(timeLeft === 0) {
-      clearInterval(timerInterval);
-      // display game over
-    }
-
-  }, 1000);
-}
-
-
-//Answer Selectors
-
-var answerText01 = document.createElement("button")
-var answerText02 = document.createElement("button")
-var answerText03 = document.createElement("button")
-var answerText04 = document.createElement("button")
-
-
-//attempting to have possible answers displayed in the buttons.  ! not working !
-// answerText01.textContent = (questions[0].answerOptions[0])
-answerText02.textContent = "Working"
-answerText03.textContent = "Working"
-answerText04.textContent = "Working"
-
-body.appendChild(answerText01)
-body.appendChild(answerText02)
-body.appendChild(answerText03)
-body.appendChild(answerText04)
-
-//Questions
-
-//Loop for questions -- ! not working yet !
-function questionSet() {      
-  for (var i = 0; i < questions.length; i++) {
-  questions[i]++
-  }
-}
-
-
-
-var questions = [       //Array of questions
+var questions = [             //Array of questions
   {q: "Question 1",
   answerOptions: ["A", "B", "C", "D"],
     a: "A"
@@ -86,10 +37,61 @@ var questions = [       //Array of questions
 ]
 
 
-console.log(questions[0].q) //targeting question 1
-console.log(questions[1].q) //targeting question 2
 
-console.log(questions[0].a) // targets correct answer 
+
+//Timer   WORKING, Hooray!
+
+function countdown() {
+  var timeLeft = 120;
+
+  var timerInterval = setInterval(function() {
+    timeLeft--;
+    timeElement.textContent = timeLeft;
+
+    if(timeLeft === 0) {
+      clearInterval(timerInterval);
+      // display game over
+    }
+
+  }, 1000);
+}
+
+
+// Selectors
+
+var answerText01 = document.createElement("button")
+var answerText02 = document.createElement("button")
+var answerText03 = document.createElement("button")
+var answerText04 = document.createElement("button")
+
+
+// Answer button options
+
+answerText01.textContent = (questions[0].answerOptions[0])
+answerText02.textContent = (questions[0].answerOptions[1])
+answerText03.textContent = (questions[0].answerOptions[2])
+answerText04.textContent = (questions[0].answerOptions[3])
+
+body.appendChild(answerText01)
+body.appendChild(answerText02)
+body.appendChild(answerText03)
+body.appendChild(answerText04)
+
+//Questions
+
+//Loop for questions -- ! not working yet !
+function questionSet() {      
+  for (var i = 0; i < questions.length; i++) {
+  questions[i]++
+  }
+}
+
+
+
+//console.log(questions[0].q) //targeting question 1
+//console.log(questions[1].q) //targeting question 2
+
+//console.log(questions[0].a) // targets correct answer 
 console.log(questions[1].a) // targets correct answer
 
 
